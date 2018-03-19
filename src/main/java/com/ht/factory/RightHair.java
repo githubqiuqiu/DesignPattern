48f8e08366f5d5a81107fa90aa9245c43b04370d
githubqiuqiu <36071872+githubqiuqiu@users.noreply.github.com>
2018-03-19 19:14:18 +0800

    右偏分发型

---
+++ src/main/java/com/ht/factory/RightHair.java
@@ -0,0 +1,13 @@
+package com.ht.factory;
+
+/**
+ * 右偏分发型
+ * @auth Qiu
+ * @time 2018/3/19
+ **/
+public class RightHair  implements  HairInterface{
+    @Override
+    public void draw() {
+        System.out.println("-----------------右偏分发型-------------------");
+    }
+}
