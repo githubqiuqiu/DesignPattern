4f4e50e73bcbb3832d2833af2b37ab2dbbbf0de6
githubqiuqiu <36071872+githubqiuqiu@users.noreply.github.com>
2018-03-19 19:14:46 +0800

    测试

---
+++ src/main/java/com/ht/factory/TestFactory.java
@@ -0,0 +1,16 @@
+package com.ht.factory;
+
+/**
+ * 模拟客户端实现
+ * @auth Qiu
+ * @time 2018/3/19
+ **/
+public class TestFactory {
+
+    public static void main(String[] args) {
+
+        HairInterface left = new LeftHair();
+        left.draw();
+    }
+
+}
